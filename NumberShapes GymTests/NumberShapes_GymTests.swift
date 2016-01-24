849f6dfa6855e1e8e3e9ef78e2c1ea17de82cd8c
Andrew Fenner <andrewfenner@numbershapes.com>
2016-01-23 20:50:55 -0500

    Initial Commit

---
+++ NumberShapes GymTests/NumberShapes_GymTests.swift
@@ -0,0 +1,36 @@
+//
+//  NumberShapes_GymTests.swift
+//  NumberShapes GymTests
+//
+//  Created by Andrew Fenner on 1/23/16.
+//  Copyright © 2016 Andrew Fenner. All rights reserved.
+//
+
+import XCTest
+@testable import NumberShapes_Gym
+
+class NumberShapes_GymTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
